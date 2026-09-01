b40c3aa1e77de8fa06c1cdbf0521ebb0e5f443b6
Peace <264323015+peacelover3@users.noreply.github.com>
2026-09-01 10:43:55 +0500

    ..

--- game.js
+++ game.js
@@ -44,7 +44,7 @@ const Game = {
             ship: document.getElementById('ship-name')
         };
         
-        if (els.commander) els.commander.textContent = config.commanderName || 'Ariba Zaheer';
+        if (els.commander) els.commander.textContent = config.commanderName || 'peacelover3';
         if (els.squad) els.squad.textContent = config.squadName || 'Warbringers';
         if (els.ship) els.ship.textContent = config.shipName || 'Urran Khatola';
     },
